6b92caae1c9c80e60856ed5b1caa97abf0968c8c
Madhavanmaddyy <123614281+Madhavanmaddyy@users.noreply.github.com>
2023-02-01 10:01:19 +0530

    Week2 day4

---
+++ PrimeNumber.java
@@ -0,0 +1,27 @@
+package week2.day1;
+
+public class PrimeNumber {
+	public static void main(String[] args) {
+		int i=2;
+		boolean t=false;
+		for(int a=13; i<a;i++) {
+			if (a%i==0)
+			  t=true;
+			
+				
+			
+		}
+	    if(t==false) {
+	    
+			System.out.println("13 is a prime number");
+	    }	
+        else {
+			System.out.println("13 is not prime number");
+	    
+        }
+		
+			
+		
+	}
+
+}
